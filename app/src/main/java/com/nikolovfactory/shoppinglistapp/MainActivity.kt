bab822d1a0cacca953995d76849f503de469a2f1
atanas-nikolov <master89q@gmail.com>
2024-09-26 23:14:35 +0300

    add Column, Button and LazyColumn for Add Item options

--- app/src/main/java/com/nikolovfactory/shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/nikolovfactory/shoppinglistapp/MainActivity.kt
@@ -6,14 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nikolovfactory.shoppinglistapp.ui.theme.ShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,6 +33,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
+                    var sItems by remember{ mutableStateOf(listOf<ShoppingItem>()) }
                     Column(
                         modifier = Modifier.fillMaxSize(),
                         verticalArrangement = Arrangement.Center
@@ -33,9 +42,11 @@ class MainActivity : ComponentActivity() {
                         Button(
                             onClick = {},
                             modifier = Modifier.align(Alignment.CenterHorizontally)
-                            ) {
+                        ) {
                                 Text("Add Item")
-
+                        }
+                        LazyColumn(modifier = Modifier.fillMaxSize().padding(16.dp)
+                        ) {
 
                         }
                     }
@@ -44,3 +55,9 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+data class ShoppingItem (val id: Int,
+                         var name: String,
+                         var quantity: Int,
+                         var isEditing: Boolean = false
+)
