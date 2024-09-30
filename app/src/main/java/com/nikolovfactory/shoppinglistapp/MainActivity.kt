a9e67beac2d6e14110bd03ccce5cfad410c4b0ea
atanas-nikolov <master89q@gmail.com>
2024-09-30 12:55:33 +0300

    add new class ShoppingList

--- app/src/main/java/com/nikolovfactory/shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/nikolovfactory/shoppinglistapp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -33,31 +34,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-                    var sItems by remember{ mutableStateOf(listOf<ShoppingItem>()) }
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Button(
-                            onClick = {},
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
-                        ) {
-                                Text("Add Item")
-                        }
-                        LazyColumn(modifier = Modifier.fillMaxSize().padding(16.dp)
-                        ) {
-
-                        }
-                    }
+                    ShoppingListApp()
                 }
             }
         }
     }
 }
-
-data class ShoppingItem (val id: Int,
-                         var name: String,
-                         var quantity: Int,
-                         var isEditing: Boolean = false
-)
